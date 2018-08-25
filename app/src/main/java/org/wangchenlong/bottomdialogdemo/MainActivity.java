81503aa70127ed9eefe4ed486f9dc6ba191f9328
yahxdxj <muyizishao@126.com>
2018-08-25 21:13:07 +0800

    Initial commit

---
+++ app/src/main/java/org/wangchenlong/bottomdialogdemo/MainActivity.java
@@ -0,0 +1,22 @@
+package org.wangchenlong.bottomdialogdemo;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.btn_dialog).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fm = getSupportFragmentManager();
+                BottomDialogFragment editNameDialog = new BottomDialogFragment();
+                editNameDialog.show(fm, "fragment_bottom_dialog");
+            }
+        });
+    }
+}
